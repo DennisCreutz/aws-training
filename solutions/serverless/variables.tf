632135768e9088df5922b613f4e980095591cb0b
dcreutz <dennis.creutz@prodyna.com>
2021-10-02 17:52:00 +0200

    Removed unused variables / Changed stage to 'prod'

--- solutions/serverless/variables.tf
+++ solutions/serverless/variables.tf
@@ -1,10 +1,8 @@
 locals {
-  stage         = "training"
+  stage         = "prod"
   project       = "aws-training"
   function_name = "trump-quote"
   file_name     = "${local.function_name}.zip"
-  headers       = "Content-Type, X-Amz-Date, Authorization, X-Api-Key ,X-Amz-Security-Token"
-  methods       = "OPTIONS, GET, POST, PUT"
   prefix_env    = terraform.workspace == "default" ? local.stage : terraform.workspace
   prefix        = "${local.project}-${local.prefix_env}"
   default_tags = {
